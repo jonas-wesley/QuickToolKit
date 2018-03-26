9f7bd63fbd36dffb389e8fea7acd1d5aebcffeec
Jonas Wesley <east_softwares@outlook.com>
2018-03-26 18:44:24 -0300

    Update module.gd

--- Qtoolkit/modules/public/module.gd
+++ Qtoolkit/modules/public/module.gd
@@ -4,14 +4,5 @@ var colors
 
 func _ready():
 	colors = require_library("misc/colors.gd")
-
-func returnNum(num):
-	return num * num
-
-func _http_connect(host, port):
-	var http = HTTPClient.new();
-	http.connect(host, port)
-	return http
-
 func _init():
 	print("Module initialized")
